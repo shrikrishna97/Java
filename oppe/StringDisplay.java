69c4f75391a99a9a97ba115c3453ae16b61de94f
Shri Krishna Pandey <96811526+shrikrishna97@users.noreply.github.com>
2022-06-16 20:51:56 +0530

    Update StringDisplay.java

--- oppe/StringDisplay.java
+++ oppe/StringDisplay.java
@@ -7,8 +7,33 @@ public static void main(String[] agrs){
   for (int i =0;i<s.length();i++){
     a.add(s.charAt(i));
 }
-  for(Character ai: a){
-  System.out.print(ai);
-  }
+//     method 1
+  // for(Character ai: a){
+  // System.out.print(ai);
+  // }
+    
+//     method 2
+  // Iterator<Character> it = a.iterator();
+  // while(it.hasNext()){
+  //   System.out.print(it.next());
+  // }
+
+//     method 3
+  // for(var ai:a){
+  //   System.out.print(ai);
+  // }
+
+//     method 4
+  // this won't work for LinkedHashSet<character> cuz List,Set are child of iterator
+//   for(ListIterator<Character> it = a.listIterator(); it.hasNext();){
+//     System.out.print(it.next());
+//   }
+
+//      method 5   
+  // a.forEach((it)->{System.out.print(it);});
+
+//     mwthod 6
+  a.forEach(System.out::print);
+
+ }
 }
-} 
